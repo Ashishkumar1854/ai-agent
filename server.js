e54bb154172aae932114338084e6a2eb45d2e748
Ashish Kumar <kashish84396@gmail.com>
2025-12-14 23:25:10 +0530

    agent done

--- server.js
+++ server.js
@@ -6,12 +6,53 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { AgentExecutor, createToolCallingAgent } from "@langchain/core/agents";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { DynamicStructuredTool } from "@langchain/core/tools";
+import { z } from "zod";
 
 // Gemini model
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-1.5-flash",
   temperature: 0.5,
   apiKey: process.env.GOOGLE_API_KEY,
+});
+
+// Tool
+const getMenuTool = new DynamicStructuredTool({
+  name: "getMenuTool",
+  description:
+    "Returns the final answer for today's menu for the given category (breakfast, lunch, or dinner). Use this tool to directly answer the user's menu question.",
+  schema: z.object({
+    category: z
+      .string()
+      .describe("Type of food. Example: breakfast, lunch, dinner"),
+  }),
+  func: async ({ category }) => {
+    const menus = {
+      breakfast: "Allo paratha, Poha, Masala Chai",
+      lunch: "Paneer Butter Masala, Dal Fry, Jeera Rice, Roti",
+      dinner: "Veg Biryani, raita, Salad, Gulab jamun",
+    };
+    return menus[category.toLowerCase()] || "No menu found for that category.";
+  },
+});
+
+//prompt
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "you are a helpful assistant that uses tools when needed."],
+  ["human", "{input}"],
+  ["ai", "{agent_scratchpad}"],
+]);
+
+// Agent
+const agent = await createToolCallingAgent({
+  llm: model,
+  tools: [getMenuTool],
+  prompt,
+});
+
+// Executor
+const executor = await AgentExecutor.fromAgentTools({
+  agent,
+  tools: [getMenuTool],
 });
 
 const app = express();
